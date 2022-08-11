908e807ee1cb3ca1711583b0024640d99fcd7065
Dziewczyna <joanna.podkoscielna@gmail.com>
2022-08-11 19:57:15 +0200

    Initial commit od Pizza-cloud application

---
+++ src/test/java/com/pizzacloud/pizza/PizzaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzacloud.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
